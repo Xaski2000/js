22391315d04e16c406f5f758035245665ac98e8b
Xaski <mr.xaski.2002@gmail.com>
2020-11-27 16:36:30 +0700

    Третье часть, первый номер

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 // 'use strict';
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-if (numberOfFilms < 10 && numberOfFilms > 0) {
-	console.log("Просмотрено довольно мало фильмов");
-} else { if (numberOfFilms >= 10 && numberOfFilms <= 30) {
-	console.log("Вы классический зритель");
-} else { if (numberOfFilms >30) {
-	console.log("Вы киноман");
-} else { console.log("Произошла ошибка");
-}
-}
+let numberOfFilms;
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	}
 }
 
-
+start();
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -22,16 +17,34 @@ const personalMovieDB = {
 	privet: false
 };
 
-for (let i = 0; i < 2; i++) {
-	const a = prompt('Один из последних просмотренных фильмов?', ''),
-			b = prompt('На сколько оцените его?', '');
-	if (a != '' && b != '' && a != null && b != null && a.length < 50 ){
-		console.log('done');
-		personalMovieDB.movies[a] = b;
-	}else{
-		console.log('error');
-		i--;
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из последних просмотренных фильмов?', ''),
+				b = prompt('На сколько оцените его?', '');
+		if (a != '' && b != '' && a != null && b != null && a.length < 50 ){
+			console.log('done');
+			personalMovieDB.movies[a] = b;
+		}else{
+			console.log('error');
+			i--;
+		}
 	}
 }
+
+rememberMyFilms();
+
+function detectPersonsalLevel() {
+	if (numberOfFilms < 10 && numberOfFilms > 0) {
+		console.log("Просмотрено довольно мало фильмов");
+	} else { if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+		console.log("Вы классический зритель");
+		} else { if (numberOfFilms >30) {
+			console.log("Вы киноман");
+			} else {console.log("Произошла ошибка");}
+		}
+	}
+}
+
+detectPersonsalLevel();
 
 console.log(personalMovieDB);
